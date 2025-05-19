6a53cc1c8bfc07e2fd6278a0690d888c47b16c3e
Damian Gutowski <kokoloko138@gmail.com>
2025-05-19 18:12:25 +0200

    Add UserService

--- src/main/java/pl/dgutowski/csl_lib_manager/user/UserService.java
+++ src/main/java/pl/dgutowski/csl_lib_manager/user/UserService.java
@@ -1,45 +1,21 @@
 package pl.dgutowski.csl_lib_manager.user;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import pl.dgutowski.csl_lib_manager.exception.ExceptionResponse;
+import pl.dgutowski.csl_lib_manager.user.dto.UserDTO;
+import pl.dgutowski.csl_lib_manager.utils.DtoUtils;
 
 import java.util.List;
 
 @Service
+@RequiredArgsConstructor
 public class UserService {
 
-    @Autowired
-    UserRepository userRepository;
+    private final UserRepository userRepository;
 
     public List<UserDTO> getAllUsers() {
         return userRepository.findAll()
                 .stream()
-                .map(this::toUserDTO).toList();
+                .map(DtoUtils::userToDto).toList();
     }
-
-    public UserDTO createNewUser(UserDTO body) {
-        if(userRepository.existsByEmail(body.getEmail())) {
-            throw new ExceptionResponse(HttpStatus.CONFLICT, "REGISTRATION_FAILED");
-        }
-
-        User saved = userRepository.save(
-                User.builder()
-                        .email(body.getEmail())
-                        .password("")
-                        .name(!body.getName().isBlank() ? body.getName() : null)
-                        .build()
-        );
-
-        return toUserDTO(saved);
-    }
-
-    public UserDTO toUserDTO(User user) {
-        return UserDTO.builder()
-                .email(user.getEmail())
-                .name(user.getName())
-                .build();
-    }
-
 }
