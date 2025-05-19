afcec9c4cd89ae15107a4364f8bef13ba5fbc32c
Damian Gutowski <kokoloko138@gmail.com>
2025-05-19 18:12:06 +0200

    Add User repository

--- src/main/java/pl/dgutowski/csl_lib_manager/user/UserRepository.java
+++ src/main/java/pl/dgutowski/csl_lib_manager/user/UserRepository.java
@@ -1,9 +1,16 @@
 package pl.dgutowski.csl_lib_manager.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface UserRepository extends JpaRepository<User, UUID> {
     Boolean existsByEmail(String email);
+    Optional<User> findByEmail(String email);
+
+    @Query("SELECT u.role FROM User u WHERE u.email=:email")
+    UserRole findRoleByEmail(@Param("email") String email);
 }
